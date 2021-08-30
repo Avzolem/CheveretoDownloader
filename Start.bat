a24b2b32512024a01fde724059a2dc1db73ec866
MandoCoding <88238944+MandoCoding@users.noreply.github.com>
2021-08-30 01:34:11 +0100

    Add files via upload

--- Start.bat
+++ Start.bat
@@ -1,18 +1,2 @@
-@echo TITLE: Chevereto Downloader.
-                                            
-@echo Joint operation by some lovely people on SE initially written for Putme.ga but should support all Chevereto based sites (untested on sites with infinite scroll) 
-
-@echo It will now try to install required modules. 
-
-pause 
-
-python -m pip install --upgrade pip
-
-python -m pip install requests beautifulsoup4
-
-@echo modules installed
-
-python "CheveretoDownloader.py"
-
-
-
+py -m pip install -r requirements.txt  --user
+py ChevertoDownloader.py
---
+++ settings.py
@@ -0,0 +1,9 @@
+# Change at own risk, values here are default
+# Defaults:
+# threads = 0 (Will run all threads), **this is not the same as cores.**
+# download_folder = "./Downloads/" will download in the CheveretoDownloader folder
+# file_attempts = 0 (will try infinitely), making it 5 as an example will try to download 5 times and then skip.
+
+threads = 0
+download_folder = "./Downloads/"
+file_attempts = 0
